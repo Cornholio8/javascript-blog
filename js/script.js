28c65d9a2655124f90fc032b772c3558e54395d6
Piotr Kamecki <piotrkamecki8@gmail.com>
2022-07-28 23:05:47 +0200

    modify script.js - add class 'active' to clicked link

--- js/script.js
+++ js/script.js
@@ -5,22 +5,30 @@
   });*/
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log('event', event);
-    /* remove class 'active' from all article links  */
+    /* [Done] remove class 'active' from all article links  */
+
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    
+    clickedElement.classList.add('active')
+    console.log('clickedElement:', clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
+
     const activeArticles = document.querySelectorAll('.posts .post');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
+
     /* get 'href' attribute from the clicked link */
 
     /* find the correct article using the selector (value of 'href' attribute) */
