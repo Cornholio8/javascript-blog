eb9154249fb35215c5bedf4d62e8cf127c6a4920
Piotr Kamecki <piotrkamecki8@gmail.com>
2022-09-07 19:05:57 +0200

    generate tag list in right column

--- js/script.js
+++ js/script.js
@@ -53,7 +53,8 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
-    optArticleAuthorSelector = '.post-author';
+    optArticleAuthorSelector = '.post-author',
+    optTagsListSelector = '.tags.list';
 
   function generateTitleLinks(customSelector = ''){
 
@@ -105,6 +106,9 @@
 
   function generateTags(){
 
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
+
     /* [DONE] find all articles */
 
     const articles = document.querySelectorAll(optArticleSelector);
@@ -146,6 +150,16 @@
 
         html = html + linkHTML;
 
+        /* [NEW] check if this link is NOT already in allTags */
+
+        if(!allTags[tag]) {
+
+          /* [NEW] add tag to allTags object */
+
+          allTags[tag] = 1;
+        } else {
+          allTags[tag]++;
+        }
       /* [DONE] END LOOP: for each tag */
       }
       /* [DONE] insert HTML of all the links into the tags wrapper */
@@ -154,6 +168,23 @@
 
     /* [DONE] END LOOP: for every article: */
     }
+
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(optTagsListSelector);
+
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += '<li><a href="#tag-' + tag + '"> ' + tag + ' (' + allTags[tag] + ') ' + '</a></li>';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /* [NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+    console.log(allTags);
   }
 
   generateTags();
